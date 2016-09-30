2049a2977baee9a7c63462ad683edb124ce48fa6
Joe Thorley <joe@poissonconsulting.ca>
2016-09-30 12:18:44 -0700

    Clean Up namespace

--- R/namespace.R
+++ R/namespace.R
@@ -1,7 +1,6 @@
 #' @import datacheckr ggplot2
 #' @importFrom envreportutils theme_soe
-#' @importFrom grDevices col2rgb dev.off graphics.off png rgb
-#' @importFrom graphics axis box image mtext par plot
+#' @importFrom grDevices dev.off png
 #' @importFrom magrittr %<>% %>%
 #' @importFrom scales comma percent
 #' @importFrom yesno yesno
