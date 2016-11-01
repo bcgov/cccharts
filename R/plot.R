eeaa576166299f46474b4c6a784311183a3311b0
Joe Thorley <joe@poissonconsulting.ca>
2016-11-01 11:52:53 -0700

    theme update

--- R/plot.R
+++ R/plot.R
@@ -216,26 +216,36 @@ trend_estimates_pngs <- function(
 #' Theme
 #'
 #' ggplot2 theme for cccharts plots
+#'
 #' @param facet A flag indicating whether to use the theme for facetted graphs.
 #' @param map A flag indicating whether to use the theme for maps.
 #' @seealso \code{\link[envreportutils]{theme_soe}} and
 #'  \code{\link[envreportutils]{theme_soe_facet}}
 #' @export
 theme_cccharts <- function(facet = FALSE, map = FALSE) {
-  ifelse(facet,
-         envreportutils::theme_soe_facet(base_family = ""),
-         envreportutils::theme_soe(base_family = "")) +
-    theme(plot.title = element_text(size = rel(1.2)),
-          axis.title.y = element_text(size = 13),
-          axis.title.x = element_blank(),
-          axis.line = element_blank(),
-          panel.grid.major.x = element_blank(),
-          panel.grid.minor.y = element_line(),
-          panel.border = element_rect(colour = "grey50", fill = NA),
-          panel.background = element_rect(colour = "grey50", fill = NA),
-          legend.position = ("bottom"),
-          legend.direction = ("horizontal")) +
-    ifelse(map, theme(panel.grid = element_blank(), panel.border = element_blank(),
-                      panel.background = element_rect(color = "white", fill = NA),
-                      axis.title = element_blank()), NULL)
+  if (facet) {
+    theme <- envreportutils::theme_soe_facet(base_family = "")
+  } else
+    theme <- envreportutils::theme_soe(base_family = "")
+
+  theme <- ggplot2::theme_update(
+    plot.title = element_text(size = rel(1.2)),
+    axis.title.y = element_text(size = 13),
+    axis.title.x = element_blank(),
+    axis.line = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.y = element_line(),
+    panel.border = element_rect(colour = "grey50", fill = NA),
+    panel.background = element_rect(colour = "grey50", fill = NA),
+    legend.position = ("bottom"),
+    legend.direction = ("horizontal"))
+
+  if (map) {
+    theme <- theme_update(
+      panel.grid = element_blank(),
+      panel.border = element_blank(),
+      panel.background = element_rect(color = "white", fill = NA),
+      axis.title = element_blank())
+  }
+  theme
 }
