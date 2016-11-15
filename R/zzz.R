c900df1dea768a993b64fb7ba9ba797a331826dd
Joe Thorley <joe@poissonconsulting.ca>
2016-11-15 07:36:52 -0800

    brewer colors

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
+  # colors from http://colorbrewer2.org/#type=diverging&scheme=RdYlBu&n=11
   op.cccharts <- list(
-    cccharts.low = "blue",
-    cccharts.mid = "yellow",
-    cccharts.high = "red")
+    cccharts.low = "#313695",
+    cccharts.mid = "#ffffbf",
+    cccharts.high = "#a50026")
   toset <- !(names(op.cccharts) %in% names(op))
   if (any(toset)) options(op.cccharts[toset])
 
