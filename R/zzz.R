64bbe0ea13a1c43fc1903b9e8a24a601ef9a3d22
Joe Thorley <joe@poissonconsulting.ca>
2016-11-14 18:44:09 -0800

    colors from brewer.pal(3, "RdYlBu")

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
+  # colors from RColorBrewer::brewer.pal(3, "RdYlBu")
   op.cccharts <- list(
-    cccharts.low = "blue",
-    cccharts.mid = "yellow",
-    cccharts.high = "red")
+    cccharts.low = "#FC8D59",
+    cccharts.mid = "#FFFFBF",
+    cccharts.high = "#91BFDB")
   toset <- !(names(op.cccharts) %in% names(op))
   if (any(toset)) options(op.cccharts[toset])
 
