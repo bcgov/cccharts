d8c2c19b0482a7993469f365eb080af50436caaa
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2016-11-29 16:44:47 -0800

    reorder bars in snow plots

--- demo/cccharts.R
+++ demo/cccharts.R
@@ -1,21 +1,27 @@
 library(cccharts)
+library(magrittr)
 
 ### sea level ####
-estimates <- plot_estimates_pngs(data = cccharts::sea_level_station, x = "Station",
+
+## sea level annual estimates map
+## set ggrepel::geom_text_repel() min.segment.length = unit(0.5, "lines") for this map in map-estimates.R
+map_estimates_pngs(data = cccharts::sea_level_station, station = TRUE, bounds = c(0.1,0.65,0,0.55),
+                          width = 500L, height = 500L, low = "#8c510a", mid = "#f5f5f5", high = "#2166ac", ask = FALSE)
+
+## sea level annual estimates plot
+estimates.sl <- plot_estimates_pngs(data = cccharts::sea_level_station, x = "Station",
                                  width = 500L, height = 500L, ybreaks = seq(-15,15,by = 5),
                                  low = "#8c510a", mid = "#f5f5f5", high = "#2166ac", ask = FALSE)
 
-map <- map_estimates_pngs(data = cccharts::sea_level_station, station = TRUE, bounds = c(0.1,0.65,0,0.55),
-                          width = 500L, height = 500L, low = "#8c510a", mid = "#f5f5f5", high = "#2166ac", ask = FALSE)
-
-sl_estimates <- estimates[[1]] +
+## tweaking theme of sea level estimates plot
+estimates.sl[[1]] <- estimates.sl[[1]] +
   theme(plot.margin = unit(c(13,10,10,0),"mm"))
-plot(sl_estimates)
+plot(estimates.sl[[1]])
 
-##print sea level estimates to PNG
+##print sea level estimates plot to PNG
 png(filename = "cccharts/estimates/sea_level_station/Sea_Level_estimates.png",
     width = 500, height = 500, units = "px")
-sl_estimates
+estimates.sl[[1]]
 dev.off()
 
 ### multiplot sea level map and sea level estimates plots ###
@@ -23,33 +29,42 @@ dev.off()
 # envreportutils::multiplot(map[[1]], estimates[[1]], cols = 2, widths = c(3,1))
 # dev.off()
 
+
 ### SST ####
 
+## SST annual estimates plot
 sea_surface_temperature_station <- dplyr::filter(sea_surface_temperature_station, Season == "Annual")
+
 
 estimate_sst <- plot_estimates_pngs(data = sea_surface_temperature_station, x = "Station", geom = "bar", ask = FALSE,
                     width = 520L, height = 500L, low = "#f5f5f5", mid = NULL, high = "#08519c")
 
-sst_estimates <- estimate_sst[[1]] +
+## tweaking theme of SST annual estimates plot
+estimate_sst[[1]] <- estimate_sst[[1]] +
   theme(plot.margin = unit(c(17,10,10,0),"mm"),
         axis.text.x = element_text(size = 12))
-plot(sst_estimates)
+plot(estimate_sst[[1]])
 
-##print sea level estimates to PNG
+##print SST annual estimates to PNG
 png(filename = "cccharts/estimates/sea_surface_temperature_station/Sea_Surface_Temperature_estimates.png",
     width = 520, height = 500, units = "px")
-sst_estimates
+estimate_sst[[1]]
 dev.off()
 
+## SST seasonal estimates facet plot
+plot_estimates_pngs(data = dplyr::filter(cccharts::sea_surface_temperature_station, Season != "Annual"),
+                    x = "Season", facet = "Station", low = "#6baed6", mid = NULL, high = "#6baed6", ask = FALSE,
+                    width = 800L, height = 600L, dir = "sea_surface_temperature_station", prefix = "Seasonal")
 
-# separate Departure Bay and Entrance Island map points
+## SST annual estimates map
+## separate Departure Bay and Entrance Island map points
 sea_surface_temperature_station$Latitude[sea_surface_temperature_station$Station == "Departure Bay"] <- 49.07
 sea_surface_temperature_station$Longitude[sea_surface_temperature_station$Station == "Departure Bay"] <- -123.91
 
 sea_surface_temperature_station$Latitude[sea_surface_temperature_station$Station == "Entrance Island"] <- 49.25
 sea_surface_temperature_station$Longitude[sea_surface_temperature_station$Station == "Entrance Island"] <- -123.9
 
-# map SST station points based on Google Map lat/long coordinates
+## map SST station points based on Google Map lat/long coordinates
 sea_surface_temperature_station$Latitude[sea_surface_temperature_station$Station == "Race Rocks"] <- 48.31
 sea_surface_temperature_station$Longitude[sea_surface_temperature_station$Station == "Race Rocks"] <- -123.53
 
@@ -62,12 +77,11 @@ sea_surface_temperature_station$Longitude[sea_surface_temperature_station$Statio
 sea_surface_temperature_station$Latitude[sea_surface_temperature_station$Station == "Pine Island"] <- 50.98
 sea_surface_temperature_station$Longitude[sea_surface_temperature_station$Station == "Pine Island"] <- -127.72
 
+
+## set ggrepel::geom_text_repel() min.segment.length = unit(0.4, "lines") for this map in map-estimates.R
 map_estimates_pngs(data = sea_surface_temperature_station, station = TRUE, bounds = c(0.1,0.65,0,0.5),
                    width = 500L, height = 500L, low = "#f5f5f5", mid = NULL, high = "#08519c",ask = FALSE)
 
-plot_estimates_pngs(data = dplyr::filter(cccharts::sea_surface_temperature_station, Season != "Annual"),
-                    x = "Season", facet = "Station", low = "#6baed6", mid = NULL, high = "#6baed6", ask = FALSE,
-                    width = 800L, height = 600L, dir = "sea_surface_temperature_station", prefix = "Seasonal")
 
 ### flow timing ####
 
@@ -93,16 +107,38 @@ plot_fit_pngs(data = flow_station_discharge, observed = cccharts::flow_station_d
 
 plot_fit_pngs(data = flow_station_discharge, observed = cccharts::flow_station_discharge_observed, by = "Station", width = 300L, height = 300L, xbreaks = seq(1950, 2010,by = 10), ask = FALSE)
 
+
 ### snow ###
 
-plot_estimates_pngs(data = cccharts::snow_station, ybreaks = seq(-20,10,by = 5),
-                    low = "#8c510a", mid = "#f5f5f5", high = "#2166ac",
-                    width = 900L, height = 500L, ask = FALSE)
+# plot_estimates_pngs(data = cccharts::snow_station, ybreaks = seq(-20,10,by = 5),
+#                     low = "#8c510a", mid = "#f5f5f5", high = "#2166ac",
+#                     width = 900L, height = 500L, ask = FALSE)
 
-plot_estimates_pngs(data = cccharts::snow, ybreaks = seq(-20,10,by = 5),
+## snow annual ecoprovince estimates
+
+## Remove CIs for plotting
+snow_estimates_data <- dplyr::select(cccharts::snow, -(c(Lower, Upper))) %>%
+                  dplyr::arrange(Indicator, Estimate)
+
+
+snow_estimates_plot <- plot_estimates_pngs(data = snow_estimates_data, ybreaks = seq(-20,10,by = 5), geom = "bar",
                     low = "#8c510a", mid = "#f5f5f5", high = "#2166ac",
                     width = 500L, height = 500L, ask = FALSE)
 
+## Lists for reordering bars by Estimate
+depth_order <- c("SI", "CI", "SIM","GD", "BP", "CM",  "SBI", "TP","NBM")
+swe_order <- c("SI", "CI", "SIM","GD", "BP", "SBI",  "CM", "TP","NBM")
+
+## reorder bars in plots
+snow_estimates_plot[[1]] <- snow_estimates_plot[[1]] +
+  scale_x_discrete(limits = depth_order)
+plot(snow_estimates_plot[[1]])
+
+snow_estimates_plot[[2]] <- snow_estimates_plot[[2]] +
+  scale_x_discrete(limits = swe_order)
+plot(snow_estimates_plot[[2]])
+
+## snow annual ecoprovince estimates map
 map_estimates_pngs(data = cccharts::snow, low = "#8c510a", mid = "#f5f5f5", high = "#2166ac",
                    width = 500L, height = 550L, ask = FALSE)
 
